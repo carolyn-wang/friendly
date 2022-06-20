b2cdec0fe671226041406a8934a6afa4678fb943
Carolyn Wang <carolynw@fb.com>
2022-06-20 13:15:51 -0700

    added bottom toolbar for MainActivity with fragment manager

--- app/src/main/java/com/example/friendly/MainActivity.java
+++ app/src/main/java/com/example/friendly/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.friendly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.friendly.fragments.MatchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Context mContext;
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+    private Fragment fragment;
     private Button btnLogout;
 
     @Override
@@ -34,6 +43,32 @@ public class MainActivity extends AppCompatActivity {
                 goLoginActivity();
             }
         });
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new MatchFragment();
+                        break;
+                    case R.id.action_compose:
+                        fragment = new MatchFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new MatchFragment();
+                        break;
+                    default: return true;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+
+
+        });
+        // Set default fragment
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     private void goLoginActivity() {
