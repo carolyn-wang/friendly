d293ea630486dc97e6a50f22cf73b84edb9e7db8
Carolyn Wang <carolynw@fb.com>
2022-06-20 13:32:19 -0700

    moved log out button to profileFragment

--- app/src/main/java/com/example/friendly/MainActivity.java
+++ app/src/main/java/com/example/friendly/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.friendly.fragments.MatchFragment;
+import com.example.friendly.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
@@ -25,25 +26,12 @@ public class MainActivity extends AppCompatActivity {
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
     private Fragment fragment;
-    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
-
-        btnLogout = findViewById(R.id.btnLogout);
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "onClick logout button");
-                ParseUser.logOut();
-                goLoginActivity();
-            }
-        });
-
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
@@ -57,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new MatchFragment();
                         break;
                     case R.id.action_profile:
-                        fragment = new MatchFragment();
+                        fragment = new ProfileFragment();
                         break;
                     default: return true;
                 }
@@ -69,12 +57,6 @@ public class MainActivity extends AppCompatActivity {
         });
         // Set default fragment
         bottomNavigationView.setSelectedItemId(R.id.action_home);
-    }
-
-    private void goLoginActivity() {
-        Intent i = new Intent(mContext, LoginActivity.class);
-        startActivity(i);
-//        finish();
     }
 
 }
---
+++ app/src/main/java/com/example/friendly/fragments/ProfileFragment.java
@@ -0,0 +1,66 @@
+package com.example.friendly.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.friendly.LoginActivity;
+import com.example.friendly.MainActivity;
+import com.example.friendly.R;
+import com.parse.ParseUser;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "ProfileFragment";
+    private Context mContext;
+
+    private Button btnLogout;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mContext = getContext();
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick logout button");
+                ParseUser.logOut();
+                goLoginActivity();
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(mContext, LoginActivity.class);
+        startActivity(i);
+        ((MainActivity)mContext).finish();
+    }
+}
