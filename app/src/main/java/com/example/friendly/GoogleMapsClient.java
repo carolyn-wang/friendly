5c489f46e5d8e04ca3146a3cb58709ff218edc61
Carolyn Wang <carolynw@fb.com>
2022-06-22 15:42:25 -0700

    initial commit to follow another API documentation

--- app/src/main/java/com/example/friendly/GoogleMapsClient.java
+++ app/src/main/java/com/example/friendly/GoogleMapsClient.java
@@ -1,3 +1,12 @@
+import static android.provider.Settings.System.getString;
+
+import com.example.friendly.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+
 //package com.example.friendly;
 //
 //import java.io.IOException;
@@ -8,7 +17,25 @@
 //import okhttp3.RequestBody;
 //import okhttp3.Response;
 //
-//public class GoogleMapsClient {
+public class GoogleMapsClient {
+    final private static String MAPS_API_KEY = null;
+    public GoogleMapsClient() {
+    }
+
+    public getNearby(){
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIza...")
+                .build();
+        GeocodingResult[] results =  GeocodingApi.geocode(context,
+                "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(results[0].addressComponents));
+
+// Invoke .shutdown() after your application is done making requests
+        context.shutdown();
+    }
+
+
 //    OkHttpClient client = new OkHttpClient().newBuilder()
 //            .build();
 //    MediaType mediaType = MediaType.parse("text/plain");
@@ -26,4 +53,4 @@
 //            e.printStackTrace();
 //        }
 //    }
-//}
+}
