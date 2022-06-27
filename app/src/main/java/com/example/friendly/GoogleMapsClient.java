cb8e7445d02132d8dad17b5e1d8ab8d1c52e8356
Carolyn Wang <carolynw@fb.com>
2022-06-27 10:31:12 -0700

    saving changes before changing branch

--- app/src/main/java/com/example/friendly/GoogleMapsClient.java
+++ app/src/main/java/com/example/friendly/GoogleMapsClient.java
@@ -6,6 +6,13 @@ import com.google.gson.GsonBuilder;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
+import com.parse.Parse;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 //package com.example.friendly;
 //
@@ -35,6 +42,42 @@ public class GoogleMapsClient {
         context.shutdown();
     }
 
+/*
+    public class GoogleMapsClient {
+        private static final String TAG = "GoogleMapsClient";
+        public static final String SCHEME = "https";
+        public static final String REST_URL = "maps.googleapis.com"; // Change this, base API URL
+        private static final String MAPS_API_KEY = Parse.getApplicationContext().getResources().getString(R.string.google_maps_api_key);
+        private HttpUrl.Builder baseUrl;
+        private OkHttpClient client;
+
+        public GoogleMapsClient() {
+            client = new OkHttpClient().newBuilder()
+                    .build();
+
+            baseUrl = new HttpUrl.Builder()
+                    .scheme(SCHEME)
+                    .host(REST_URL)
+                    .addQueryParameter("key", MAPS_API_KEY);
+
+        }
+
+        public Request getNearbyPlaces() throws IOException {
+            HttpUrl nearbyUrl = baseUrl.addPathSegment("maps/api/place/nearbysearch/json")
+                    .addQueryParameter("location", "-33.8670522%2C151.1957362&")
+                    .addQueryParameter("radius", "1500")
+                    .addQueryParameter("type", "restaurant")
+                    .build();
+            Request request = new Request.Builder()
+                    .url(nearbyUrl)
+                    .get()
+                    .build();
+            return request;
+//        Log.i(TAG, request.toString());
+        }
+
+    }
+    */
 
 //    OkHttpClient client = new OkHttpClient().newBuilder()
 //            .build();
