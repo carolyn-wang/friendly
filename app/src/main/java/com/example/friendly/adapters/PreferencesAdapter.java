805f1c813cc917968d1505678c466e3b11b7cef8
Carolyn Wang <carolynw@fb.com>
2022-07-07 12:16:55 -0700

    added radio poll card to RV

--- app/src/main/java/com/example/friendly/adapters/PreferencesAdapter.java
+++ app/src/main/java/com/example/friendly/adapters/PreferencesAdapter.java
@@ -5,7 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,11 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.friendly.objects.Preference;
 import com.example.friendly.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.ViewHolder> {
-
     protected Context context;
     protected List<Preference> preferences;
     private static final String TAG = "PreferencesAdapter";
@@ -39,8 +38,26 @@ public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_poll_card, parent, false);
+        View view;
+        switch (viewType) {
+            case 0: view = LayoutInflater.from(context).inflate(R.layout.item_poll_card_radio, parent, false);
+                break;
+            case 2: view = LayoutInflater.from(context).inflate(R.layout.item_poll_card_checkbox, parent, false);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + viewType);
+        }
         return new ViewHolder(view);
+    }
+    @Override
+    public int getItemViewType(int position) {
+        // Just as an example, return 0 or 2 depending on position
+        // Note that unlike in ListView adapters, types don't have to be contiguous
+        if (position < 2){
+            return 0;
+        }else{
+            return 2;
+        }
     }
 
     @Override
@@ -57,14 +74,14 @@ public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.
     class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView tvQuestion;
-        private CheckBox cbOption0;
-        private CheckBox cbOption1;
-        private CheckBox cbOption2;
-        private CheckBox cbOption3;
-        private CheckBox cbOption4;
-        private CheckBox cbOption5;
-        private CheckBox cbOption6;
-        private CheckBox[] optionViews;
+        private Button cbOption0;
+        private Button cbOption1;
+        private Button cbOption2;
+        private Button cbOption3;
+        private Button cbOption4;
+        private Button cbOption5;
+        private Button cbOption6;
+        private Button[] optionViews;
 //            protected RecyclerView rvOptions;
 
 
@@ -79,14 +96,14 @@ public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.
             cbOption4 = itemView.findViewById(R.id.cbOption4);
             cbOption5 = itemView.findViewById(R.id.cbOption5);
             cbOption6 = itemView.findViewById(R.id.cbOption6);
-            optionViews = new CheckBox[]{cbOption0, cbOption1, cbOption2, cbOption3, cbOption4, cbOption5, cbOption6};
+            optionViews = new Button[]{cbOption0, cbOption1, cbOption2, cbOption3, cbOption4, cbOption5, cbOption6};
 //            rvOptions = itemView.findViewById(R.id.rvOptions);
 //                currentUser = ParseUser.getCurrentUser();
         }
 
         /**
          * Set poll question.
-         *  Set text for checkbox option or set visibility to GONE
+         *  Set text for Button option or set visibility to GONE
          * @param preference
          */
         public void bind(Preference preference) {
@@ -100,9 +117,6 @@ public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.
                     optionViews[i].setVisibility(View.GONE);
                 }
             }
-//                PollOptionsAdapter adapter = new PollOptionsAdapter(preference.getOptions());
-//                rvOptions.setAdapter(adapter);
-//                adapter.notifyDataSetChanged();
         }
     }
 
@@ -117,4 +131,8 @@ public class PreferencesAdapter extends RecyclerView.Adapter<PreferencesAdapter.
         preferences.addAll(list);
         notifyDataSetChanged();
     }
+
+
+
+
 }
