4c1a621ef1a1d7134bf000a0f63b73beadca5ca6
Carolyn Wang <carolynw@fb.com>
2022-07-18 21:16:45 -0700

    listview click windows of time

--- app/src/main/java/com/example/friendly/fragments/AvailabilityFragment.java
+++ app/src/main/java/com/example/friendly/fragments/AvailabilityFragment.java
@@ -2,11 +2,11 @@ package com.example.friendly.fragments;
 
 import android.content.ClipData;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -15,10 +15,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.friendly.R;
 
@@ -30,13 +29,18 @@ import com.example.friendly.R;
 public class AvailabilityFragment extends Fragment {
 
     private static final String TAG = "AvailabilityFragment";
-    private boolean[] availability = new boolean[]{false, false, false, false, false};
+    private boolean[] availability = new boolean[]{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    private ListView lvAvailability;
+    private ListView lvAvailability1;
+    private ConstraintLayout availabilityLayout;
+    private ConstraintLayout availabilityLayout1;
 
-    private TableRow row1;
-    private TableRow row2;
-    private TableRow row3;
-    private TableRow row4;
-    private TableLayout availabilityTable;
+
+    String[] timesArray = {"9 AM - 9:30 AM", "9:30 AM - 10 AM ", "10 AM - 10:30 AM", "10:30 AM - 11 AM",
+            "11 AM - 11:30 AM", "11:30 AM - 12 PM ", "12 PM - 12:30 PM", "12:30 PM - 1 PM",
+            "1 PM - 2 PM", "2 PM - 3 PM", "3 PM - 4 PM", "4 PM - 5 PM", "5 PM - 6 PM", "6 PM - 7 PM",
+            "7 PM - 8 PM", "8 PM - 9 PM", "9 PM - 10 PM", "10 PM - 11 PM", "11 PM - 12 AM"
+    };
 
 
     public AvailabilityFragment() {
@@ -50,105 +54,105 @@ public class AvailabilityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_availability, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        availabilityTable = view.findViewById(R.id.availabilityTable);
+        lvAvailability = view.findViewById(R.id.availabilityTable);
+        lvAvailability1 = view.findViewById(R.id.availabilityTable1);
 
-        row1 = view.findViewById(R.id.row1);
-        row2 = view.findViewById(R.id.row2);
-        row3 = view.findViewById(R.id.row3);
-        row4 = view.findViewById(R.id.row4);
-//        row5 = view.findViewById(R.id.row5);
+        availabilityLayout = view.findViewById(R.id.availabilityLayout);
 
 
-//        row1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                row1.setTag(row1.getId());
-//                int row_index = availabilityTable.indexOfChild(row1);
-//                row1.setBackgroundColor(Color.BLUE);
-//                Log.i(TAG, String.valueOf(row_index));
-//            }
-//        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                R.layout.item_availability_row, timesArray);
+        lvAvailability.setAdapter(adapter);
+        lvAvailability1.setAdapter(adapter);
 
+        lvAvailability.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                availability[position] = !availability[position];
+                if (availability[position]) {
+                    view.setBackgroundColor(getResources().getColor(R.color.light_tan));
+                } else {
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                }
+            }
+        });
 
-        row1.setOnTouchListener(new MyTouchListener());
-        row2.setOnTouchListener(new MyTouchListener());
-        row3.setOnTouchListener(new MyTouchListener());
-        row4.setOnTouchListener(new MyTouchListener());
+        lvAvailability1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                availability[position] = !availability[position];
+                if (availability[position]) {
+                    view.setBackgroundColor(getResources().getColor(R.color.light_tan));
+                } else {
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                }
+            }
+        });
 
-//        row1.setOnDragListener(new MyDragListener());
-        availabilityTable.setOnDragListener(new MyDragListener());
+        lvAvailability.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                     @Override
+                                                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                         ClipData data = ClipData.newPlainText("", "");
+                                                         View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                                                         view.startDragAndDrop(data, shadowBuilder, view, 0);
+                                                         Log.i(TAG, "select");
+                                                     }
 
-    }
+                                                     @Override
+                                                     public void onNothingSelected(AdapterView<?> parent) {
 
-    private final class MyTouchListener implements View.OnTouchListener {
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-//                ClipData data = ClipData.newPlainText("", "");
-//                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-//                view.startDrag(data, shadowBuilder, view, 0);
-                changeAvailability(view);
-                return true;
-            } else {
+                                                     }
+                                                 }
+        );
+//        lvAvailability.setOnTouchListener(new MyTouchListener());
+
+        availabilityLayout.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View view, DragEvent event) {
+                final int action = event.getAction();
+                switch (action) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.i(TAG, "drag started **");
+                        return true;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.i(TAG, "drag entered");
+                        view.setBackgroundColor(Color.GREEN);
+                        return true;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.i(TAG, "drag exited");
+                        view.setBackgroundColor(Color.BLUE);
+                        view.invalidate();
+                        return true;
+                    case DragEvent.ACTION_DROP:
+                    case DragEvent.ACTION_DRAG_ENDED:
+
+                        view.setBackgroundColor(Color.YELLOW);
+                        return true;
+                    default:
+                        break;
+                }
                 return false;
+
             }
-        }
-    }
-
-    /**
-     * Invert color and availability for given time slot.
-     * @param view Row that was just selected.
-     */
-    private void changeAvailability(View view) {
-        int rowIndex = availabilityTable.indexOfChild(view);
-        availability[rowIndex] = !availability[rowIndex];
-        if (availability[rowIndex]){
-            view.setBackgroundColor(getResources().getColor(R.color.light_tan));
-        }
-        else{
-            view.setBackgroundColor(Color.TRANSPARENT);
-        }
+        });
     }
 
 
-    class MyDragListener implements View.OnDragListener {
+    // This defines your touch listener
+    private final class MyTouchListener implements View.OnTouchListener {
         @Override
-        public boolean onDrag(View v, DragEvent event) {
-            int action = event.getAction();
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    Log.i(TAG, "drag started **");
-                    // do nothing
-                    break;
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i(TAG, "drag entered");
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i(TAG, "drag exited **");
-                    break;
-//                case DragEvent.ACTION_DROP:
-//                    // Dropped, reassign View to ViewGroup
-//                    View view = (View) event.getLocalState();
-//                    ViewGroup owner = (ViewGroup) view.getParent();
-//                    owner.removeView(view);
-//                    LinearLayout container = (LinearLayout) v;
-//                    container.addView(view);
-//                    view.setVisibility(View.VISIBLE);
-//                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    Log.i(TAG, "drag ended");
-                default:
-                    break;
-            }
+        public boolean onTouch(View view, MotionEvent event) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDragAndDrop(data, shadowBuilder, view, 0);
             return true;
         }
     }
-
 
 }
